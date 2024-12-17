323fe0e2ac386ee7f445daa4260cfb407249ad1a
naoyuki <naounaox@icloud.com>
2024-12-18 02:07:09 +0900

    Fix Google Analytics implementation

--- pages/_app.js
+++ pages/_app.js
@@ -21,45 +21,68 @@
 // }
 
 // pages/_app.js
+// import "@/styles/globals.css";
+// import { useEffect } from 'react';
+// import { useRouter } from 'next/router';
+// import Script from 'next/script';
+
+// export default function App({ Component, pageProps }) {
+//   const router = useRouter();
+
+//   useEffect(() => {
+//     const handleRouteChange = (url) => {
+//       window.gtag('config', 'G-YBQT1V7DYJ', {
+//         page_path: url,
+//       });
+//     };
+//     router.events.on('routeChangeComplete', handleRouteChange);
+//     return () => {
+//       router.events.off('routeChangeComplete', handleRouteChange);
+//     };
+//   }, [router.events]);
+
+//   return (
+//     <>
+//       {/* Google Analytics */}
+//       <Script
+//         strategy="afterInteractive"
+//         src={`https://www.googletagmanager.com/gtag/js?id=G-YBQT1V7DYJ`}
+//       />
+//       <Script
+//         id="google-analytics"
+//         strategy="afterInteractive"
+//         dangerouslySetInnerHTML={{
+//           __html: `
+//             window.dataLayer = window.dataLayer || [];
+//             function gtag(){dataLayer.push(arguments);}
+//             gtag('js', new Date());
+//             gtag('config', 'G-YBQT1V7DYJ');
+//           `,
+//         }}
+//       />
+//       <Component {...pageProps} />
+//     </>
+//   );
+// }
+
 import "@/styles/globals.css";
-import { useEffect } from 'react';
-import { useRouter } from 'next/router';
 import Script from 'next/script';
 
 export default function App({ Component, pageProps }) {
-  const router = useRouter();
-
-  useEffect(() => {
-    const handleRouteChange = (url) => {
-      window.gtag('config', 'G-YBQT1V7DYJ', {
-        page_path: url,
-      });
-    };
-    router.events.on('routeChangeComplete', handleRouteChange);
-    return () => {
-      router.events.off('routeChangeComplete', handleRouteChange);
-    };
-  }, [router.events]);
-
   return (
     <>
-      {/* Google Analytics */}
       <Script
-        strategy="afterInteractive"
         src={`https://www.googletagmanager.com/gtag/js?id=G-YBQT1V7DYJ`}
-      />
-      <Script
-        id="google-analytics"
         strategy="afterInteractive"
-        dangerouslySetInnerHTML={{
-          __html: `
-            window.dataLayer = window.dataLayer || [];
-            function gtag(){dataLayer.push(arguments);}
-            gtag('js', new Date());
-            gtag('config', 'G-YBQT1V7DYJ');
-          `,
-        }}
       />
+      <Script id="ga-script" strategy="afterInteractive">
+        {`
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+          gtag('config', 'G-YBQT1V7DYJ');
+        `}
+      </Script>
       <Component {...pageProps} />
     </>
   );
